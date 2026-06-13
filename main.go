87d6bc7e5990d76cdb6c22a1c2782f7973979b09
Alexander Zaitsev <vundonnmailps@proton.me>
2026-06-13 12:43:11 +0500

    update main.go

--- main.go
+++ main.go
@@ -8,11 +8,22 @@ import (
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello, my name is Alexandr, this is my first Go Website")
 }
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "About me: I'm Technical Support Engineer and now I'm learning Go and building my first personal website")
+}
+func projectHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Projects: Go Visit Site, Tuxback and Campus bot")
+}
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Contacts: Telegram, WhatsApp, GitHub, LinkedIn, Email")
+}
 
 func main() {
 	http.HandleFunc("/", homeHandler)
-
-	fmt.Println("Server started on localhost:http://localhost:8080")
+	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/projects", projectHandler)
+	http.HandleFunc("/contacts", contactHandler)
+	fmt.Println("Server started on http://localhost:8080")
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
